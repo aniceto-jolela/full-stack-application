2ce831ac15081a87d348325375e5ff543de07ef1
aniceto-jolela <anicetojosejolelaj@gmail.com>
2024-12-29 16:05:47 +0100

    add .env and .gitignore

--- README.md
+++ README.md
@@ -13,3 +13,45 @@ Task: Build a full-stack application to manage a user directory.
 - Implement basic security:
 - CSRF protection.
 - Validate input to prevent SQL injection.
+
+#
+
+> Steps
+1. install PostgreSQL
+    ```bash
+      sudo apt install postgresql   
+    ```
+
+    ```bash
+    psql --version
+    ```
+2. install Psycopg (to CPython)
+   - tip -> Database adapter
+   ```bash
+      pip install "psycopg[binary]"
+    ```
+3. install Dotenv
+   ```bash
+      pip install python-dotenv
+    ```
+
+
+
+#
+> Versions
+>> python 3.12.4 
+>>
+>> postgresql 14.5
+>> 
+>> pip 24.3.1
+>>
+>> psycopg 3.2.3
+>>
+>> psycopg-binary 3.2.3
+>>
+>> python-dotenv 1.0.1
+ 
+
+
+
+
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +78,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get("ENGINE_DB"),
+        "USER": os.environ.get("USER_DB"),
+        "NAME": os.environ.get("NAME_DB"),
+        "PASSWORD": os.environ.get("PASSWORD_DB"),
+        "HOST": os.environ.get("HOST"),
+        "PORT": os.environ.get("PORT_DB"),
     }
 }
 
