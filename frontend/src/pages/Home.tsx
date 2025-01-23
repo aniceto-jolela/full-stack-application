8449cd730da7c29e954ff79e3eae4f3d44866ab9
aniceto-jolela <anicetojosejolelaj@gmail.com>
2025-01-23 14:48:04 +0100

    UI home done.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,10 +6,10 @@ import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
 import { useEffect, useState } from 'react';
 import { fetchProfile, fetchUsers } from '../api/authApi';
 import { UserProps } from '../types/types';
-import { Avatar, Grid2, Paper, styled, Typography } from '@mui/material';
+import { Alert, AlertTitle, Avatar, Grid2, Paper, styled, Typography } from '@mui/material';
 import Icon from '@mdi/react';
 import { deepPurple } from '@mui/material/colors';
-import { mdiAccountTie } from '@mdi/js';
+import { mdiAccountTie, mdiApplication } from '@mdi/js';
 
 
 const otherProps: Partial<PieChartProps> = {
@@ -99,6 +99,8 @@ const Home = () => {
   
   return (
   <>
+    {formData.is_superuser && <>
+    {!error?<>
     <Stack direction="row" spacing={2}>
       <Avatar sx={{ bgcolor: deepPurple[200], }} variant="circular"  >
           <Icon path={mdiAccountTie} size={1} color={"black"}/>
@@ -128,11 +130,8 @@ const Home = () => {
         <Item sx={{paddingLeft:0}}>
         <BarChart
           sx={{'--my-custom-gradient': 'url(#GlobalGradient)',}}
-          slotProps={{
-            popper: {
-              sx: {
-                '--my-custom-gradient': 'linear-gradient(0deg, #123456, #81b2e4);',
-              },},
+          slotProps={{popper: {
+            sx: {'--my-custom-gradient': 'linear-gradient(0deg, #123456, #81b2e4);',},},
           }}
           series={[
             {
@@ -161,23 +160,60 @@ const Home = () => {
     </Typography>
     <Stack direction={{ xs: 'column', sm: 'row-reverse', md: 'row' }} spacing={{ xs: 2, sm:2, md: 6 }} sx={{textAlign: "center"}}>
       <Gauge width={100} height={100} value={totalStaff} title="Users" />
-      <Gauge sx={(theme)=>({[`& .${gaugeClasses.valueArc}`]:{fill: '#b2022b',}})} startAngle={50} endAngle={300} width={100} height={100}  value={totalElimineted} title="Elimineted" />
-      <Gauge sx={(theme)=>({[`& .${gaugeClasses.valueArc}`]:{fill: '#52b202',}})} width={100} height={100} value={totalActive} valueMin={10} title="Active" valueMax={60} />
-      <Gauge sx={(theme)=>({[`& .${gaugeClasses.valueArc}`]:{fill: '#8002b2',}})} width={100} height={100} value={totalSuper} valueMin={10} title="Super User" valueMax={60} />
+      <Gauge sx={()=>({[`& .${gaugeClasses.valueArc}`]:{fill: '#b2022b',}})} startAngle={50} endAngle={300} width={100} height={100}  value={totalElimineted} title="Elimineted" />
+      <Gauge sx={()=>({[`& .${gaugeClasses.valueArc}`]:{fill: '#52b202',}})} width={100} height={100} value={totalActive} valueMin={10} title="Active" valueMax={60} />
+      <Gauge sx={()=>({[`& .${gaugeClasses.valueArc}`]:{fill: '#8002b2',}})} width={100} height={100} value={totalSuper} valueMin={10} title="Super User" valueMax={60} />
     </Stack>
+    </> :<Alert severity="warning"><AlertTitle>Warning</AlertTitle>{error}</Alert>}
+    </>}
 
-    <Typography  variant="h5" textAlign={"center"} sx={{marginTop:2, backgroundColor: "purple"}} gutterBottom>
+    <Typography variant="h5" color='#fff' borderRadius={5} width={275} sx={{marginTop:2, marginBottom: 5 ,backgroundColor: "purple"}} gutterBottom>
+    <Icon path={mdiApplication} size={1} style={{marginLeft:7, marginBottom:-2}} />
       Full Stack Application
     </Typography>
-    <Grid2 container rowSpacing={2} columnSpacing={{ xs: 3, sm: 2, md: 2 }}  >
-      <Grid2 size={{ xs: 12,  sm: 12, md: 6 }}>
-        <Item sx={{paddingLeft:0}}>
-           Yep
+    <Typography sx={{marginBottom:2}}>
+      This application aims to manage user directory. <br/>
+      It shows how an administrator can have user access control that uses this application,
+      and this is very useful for any application that uses authentication.
+    </Typography>
+    <Typography sx={{marginBottom:2, }} fontWeight={600}>
+      For the creation of this application I used: <br/>
+    </Typography>
+    <Grid2 container rowSpacing={2} columnSpacing={{ xs: 3, sm: 2, md: 2 }} >
+      <Grid2 size={{ xs: 12,  sm: 12, md: 6 }} >
+        <Item sx={{paddingLeft:0, paddingTop:2}}>
+           <Typography variant='h6' color='secondary'>
+           Backend
+           </Typography>
+           <Typography variant='subtitle2' sx={{textAlign: "left", paddingLeft:2, paddingTop:2}} component={"p"}>
+              .Python <br/>
+              .Nodejs <br/>
+              .djangorestframework {">"} Rest <br/>
+              .djangorestframework-simplejwt {">"} Rest-sjwt <br/>
+              .psycopg[pool] {">"} Connection pools <br/>
+              .Psycopg {">"} Database adapter <br/>
+              .PostgreSQL {">"} Database <br/>
+              .django-cors-headers {">"} [Cross-Origin Resource Sharing (CORS)]<br/>
+           </Typography>
         </Item>
       </Grid2>
       <Grid2  size={{ xs: 12,  sm: 12, md: 6 }} >
-        <Item sx={{paddingLeft:0}}>
-            Nop
+        <Item sx={{paddingLeft:0,  paddingTop:2}}>
+          <Typography variant='h6' color='primary'>
+            Frontend
+          </Typography>
+           <Typography variant='subtitle2' sx={{textAlign: "left", paddingLeft:2, paddingTop:2, marginBottom:2}} component={"p"}>
+              .Vite + React (variant TypeScript) <br/>
+              .Material UI <br/>
+              .pictogrammers {">"} Icon <br/>
+           </Typography>
+           <Typography variant='h6' color='warning'>
+            Other
+          </Typography>
+          <Typography variant='subtitle2' sx={{textAlign: "left", paddingLeft:2, paddingTop:2}} component={"p"}>
+            Similar project made in Django: <br/>
+            <a href="https://django-crud-dh2q.onrender.com/" target="_blank">crud</a>
+           </Typography>
         </Item>
       </Grid2>
     </Grid2>
